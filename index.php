184b7ff421ca101f13a79ab380948b9742abb155
Jakub 'Kafkicz' Kafka <kafkic@gmail.com>
2016-12-11 15:46:49 +0100

    kommitaaaa

--- index.php
+++ index.php
@@ -5,6 +5,24 @@ $query = "SELECT * FROM `news`";
 $result = mysqli_query($con, $query );
 
 
+if (isset($_POST['news'])){
+ 	$username = stripslashes($_SESSION['username']);
+	$username = mysqli_real_escape_string($con,$_SESSION['username']);
+	$news = mysqli_real_escape_string($link, $_POST['news']);
+	$news = stripslashes($_POST['news']);
+	$addquery = "INSERT INTO ´news´ (username, news) VALUES ('$username', '$news')";
+	$addresult = mysqli_query($con, $addquery );
+		if(mysqli_query($con, $addquery)){
+  		  echo "Records added successfully.";
+  		  }
+  		   else	{
+   				 echo "ERROR: Could not able to execute $addquery. " . mysqli_error($con);
+					}
+}
+
+
+
+
 
 ?>
 <!DOCTYPE html>
@@ -30,6 +48,13 @@ $result = mysqli_query($con, $query );
  		<p><a href="unused.php">unused</a></p>
 	</div>
 	<div class="column-right">
+		<p><h1>Novinka</h1></p>
+		<p>
+		<form action="" method="post" name="add-news"><br>
+			<input type="text" name="news" placeholder="Aktualita???" required /><br>
+			<input name="submit-news" type="submit" value="Přidat aktualitu" /><br>
+		</form>
+		</p>
 		<p><?php if ($result = mysqli_query($con, $query)) {while ($row = mysqli_fetch_array($result)) {echo'<div class="news">' . "aktualita od :" . '<br>' . $row['username'] . " ----- " . $row['time'] . '<br>';printf ($row['news']);echo '<br>' . '<br>' . '</div>';};};?></p>
 	</div>
 </div>
